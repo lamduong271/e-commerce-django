05ee1f947133ba8535676b7d1f467bf22a729863
Lam Duong <lamdu@Lams-MacBook-Pro.local>
2019-06-27 10:05:04 +0300

    order sum 2

--- core/models.py
+++ core/models.py
@@ -51,6 +51,12 @@ class OrderItem(models.Model):
         return self.quantity * self.item.price
     def get_total_discount_item_price(self):
         return self.quantity * self.item.discount_price
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -60,5 +66,11 @@ class Order(models.Model):
     ordered_date = models.DateTimeField(default=date.today)
     def __str__(self):
         return self.user.username
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
+
 
 
